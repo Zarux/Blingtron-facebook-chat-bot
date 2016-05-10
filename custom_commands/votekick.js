0394f31ca6d4beee8e5a7fcea0607c46c07285ef
Joakim <joakimtangnes@gmail.com>
2016-05-10 16:22:23 +0200

    View active votes, and clear votes when kicked

--- custom_commands/votekick.js
+++ custom_commands/votekick.js
@@ -7,17 +7,33 @@ module.exports=function(threadID, args, senderId){
 	var userKick = senderId;
 	var userKickedId;
 	var userKicked;
+	var threadUsers = uf.metaData.chatUsers[threadID];
 	var myId = api.getCurrentUserID();
+	var voteSize = Object.keys(threadUsers).length-1;
 	if(kickData[threadID] == undefined){
 		kickData[threadID] = {}
 	}
+	if(args.special.indexOf("--votes") > -1){
+		var msg = "";
+		for(i in kickData[threadID]){
+			var cur = kickData[threadID];
+			msg+=cur[i].name+" "+cur[i].votes.length+"/"+voteSize+"\n";
+		}
+		if(msg==""){
+			uf.sendMessage("No votes registered",threadID);			
+		}else{
+			uf.sendMessage(msg,threadID);
+		}
+		return
+	}
+
 	if(args.special.indexOf("--user") > -1){
 		if(args.special_value.length==0){
 			uf.sendMessage(threadID,"No user specified");
 			return;
 		}
 
-		var threadUsers = uf.metaData.chatUsers[threadID];
+
 		user_kicked = uf.getSpecialValue("--user",args);
 		for(id in threadUsers){
 			if(threadUsers[id].name.toLowerCase() == user_kicked.toLowerCase()){
@@ -31,22 +47,25 @@ module.exports=function(threadID, args, senderId){
 				if(kickData[threadID][userKickedId] == undefined){
 					kickData[threadID][userKickedId] = {votes:[],name:""}
 				}
-				kickData[threadID][userKickedId].name = userKicked;
+				var cur = kickData[threadID][userKickedId];
+				cur.name = userKicked;
 
-				if(kickData[threadID][userKickedId].votes.indexOf(senderId) == -1){
+				if(cur.votes.indexOf(senderId) == -1){
 
-					kickData[threadID][userKickedId].votes.push(senderId);
+					cur.votes.push(senderId);
 
-					var voteSize = Object.keys(threadUsers).length-1;
-					uf.sendMessage("Registered vote for "+userKicked+"\n"+kickData[threadID][userKickedId].votes.length+"/"+Math.round(voteSize/2),threadID);
+					uf.sendMessage("Registered vote for:\n"+userKicked+"\n"+cur.votes.length+"/"+Math.round(voteSize/2),threadID);
 
-					if(kickData[threadID][userKickedId].votes.length >= Math.round(voteSize/2)){
+					if(cur.votes.length >= Math.round(voteSize/2)){
 
 						api.removeUserFromGroup(userKickedId,threadID,function(err){
 							if(err)console.log(err);
+							cur.votes = []
 						});
 						
 					}
+				}else{
+					uf.sendMessage("You have already voted to kick:\n"+userKicked,threadID);
 				}
 				return
 			}
