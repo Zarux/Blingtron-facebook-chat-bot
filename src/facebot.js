c1e2f020903b259aa1de85ef43df62871c9f33bf
Joakim <joakimtangnes@gmail.com>
2016-05-16 13:45:49 +0200

    Fix indentation

--- src/facebot.js
+++ src/facebot.js
@@ -1,14 +1,13 @@
-var login    = require("facebook-chat-api");
-var request  = require('request');
-var fs       = require("fs");
-var path     = require('path');
+var login	= require("facebook-chat-api");
+var request	= require('request');
+var fs		 = require("fs");
+var path	 = require('path');
 var strftime = require('strftime');
-var unirest  = require('unirest');
-var log      = require('npmlog');
-var cmdM     = require("./commands");
-var uf      = require("./util_functions.js");
+var unirest	= require('unirest');
+var log		= require('npmlog');
+var cmdM	 = require("./commands");
+var uf		= require("./util_functions.js");
 var readline = require('readline');
-delete require.cache[require.resolve('./commands.js')];
 
 var rl = readline.createInterface({
 	input: process.stdin,
@@ -41,106 +40,104 @@ function getPw(){
 	});
 }
 
+var lastCheckUsers = {}
 
 function doLogin(loginData){
 	login(loginData, function callback (err, api) {
-	    if(err) return console.error(err);
+		if(err) return console.error(err);
 
-	    var myId = api.getCurrentUserID();
-	    fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
+		var myId = api.getCurrentUserID();
+		fs.writeFileSync('appstate.json', JSON.stringify(api.getAppState()));
 
-	    api.setOptions({
-	      selfListen: true,
-	      forceLogin:true,
-	      listenEvents: true,
-	      logLevel:"silent"
+		api.setOptions({
+			selfListen: true,
+			forceLogin:true,
+			listenEvents: true,
+			logLevel:"silent"
+		});
+		var bot_config = JSON.parse(fs.readFileSync('src/config.json', 'utf8'));
+		cmdM.meta.api = api;
+		cmdM.meta.admins = bot_config.admins;
+		uf.metaData.meta.api = api;
+		uf.metaData.meta.admins = bot_config.admins;
 
-	    });
-	    var bot_config = JSON.parse(fs.readFileSync('src/config.json', 'utf8'));
-	    cmdM.meta.api = api;
-	    cmdM.meta.admins = bot_config.admins;
-	    uf.metaData.meta.api = api;
-	    uf.metaData.meta.admins = bot_config.admins;
-
-	 	log.info("Begin listening for messages");
-	    var listening = api.listen(function(err, event) {
-	    	if(err) return console.error(err);
-	    	var chatUsers = uf.metaData.chatUsers;
-	    	if(chatUsers[event.threadID] == undefined){
-		    	api.getThreadInfo(event.threadID,function(err,info){
+		log.info("Begin listening for messages");
+		var listening = api.listen(function(err, event) {
+			if(err) return console.error(err);
+			var chatUsers = uf.metaData.chatUsers;
+			var timeNow = Math.floor(Date.now() / 1000);
+			if(chatUsers[event.threadID] == undefined || lastCheckUsers[event.threadID] < timeNow - 600 ){
+				lastCheckUsers[event.threadID] == undefined ? lastCheckUsers[event.threadID] = 0 : lastCheckUsers[event.threadID] = timeNow;
+				api.getThreadInfo(event.threadID,function(err,info){
 					if(err) return err;
 					api.getUserInfo(info.participantIDs,function(err,users){
 						uf.metaData.chatUsers[event.threadID]=users;
 					});
 				});
-	    	}
-	    	//console.log(event);
-			switch(event.type) {
-			  case "message":
-	  			var serverMsg    = "";
-	  			var eventStrtime = strftime('%F %T', new Date(event.timestamp*1));
-	  			//console.log(event);
-		    	api.getUserInfo(event.senderID,function(err,ret){
-		    		if(err) return console.error(err);
-
-		    		var sender = ret[event.senderID].name;
-		    		var body   = event.body;
-
-		    		if(body==undefined && event.attachments){
-		    			body = "Attachment";
-		    		}
-		    		
-		    		var type = "MESG";
-		    		if(cmdM.meta.isCommand(body)){
-		    			type = "CMND"		
-		    		}
-		    		if(event.senderID == myId){
-		    			serverMsg=uf.prettifyMessage(false,eventStrtime,"DEBUG","Command completed");
-		    			log.info(serverMsg);
-		    		}else{
-		    			serverMsg=uf.prettifyMessage(type,eventStrtime,sender,body);
-		    			console.log(serverMsg);
-		    		}
-		    	});
-		    	if(!event.body)
-			  		return;
-			    if(event.senderID != myId) {
-			    	var command   = event.body.split(/ +(?=(?:(?:[^"]*"){2})*[^"]*$)/g);
-			    	var cmd       = command[0];
-			    	var args      = command.slice(1,command.length);
-			    	var arguments = parseArgs(args,cmd);
-			    	
-			    	if(cmdM[cmd] && cmdM[cmd].run){
-			    		cmdM[cmd].run(event.threadID,arguments,event.senderID);
-	    		 		api.markAsRead(event.threadID, function(err) {
-							if(err) console.log(err);
-				    	});
-			    	}
-			    }
-			    break;
-			  case "event":
-			  	var serverMsg    = "";
-			  	var eventStrtime = strftime('%F %T');
-			  	//console.log(event);
-			  	api.getUserInfo(event.author,function(err,ret){
-			  		if(err) return console.error(err);
-			  		serverMsg=uf.prettifyMessage("EVNT",eventStrtime,ret[event.author].name,event.logMessageBody);
-			    	console.log(serverMsg);
-			  	});
-			  	
-			    break;
 			}
-	    });
+			switch(event.type) {
+				case "message":
+					var serverMsg	= "";
+					var eventStrtime = strftime('%F %T', new Date(event.timestamp*1));
+					api.getUserInfo(event.senderID,function(err,ret){
+						if(err) return console.error(err);
+
+						var sender = ret[event.senderID].name;
+						var body	 = event.body;
+
+						if(body==undefined && event.attachments){
+							body = "Attachment";
+						}
+						var type = "MESG";
+						if(cmdM.meta.isCommand(body)){
+							type = "CMND"		
+						}
+						if(event.senderID == myId){
+							serverMsg=uf.prettifyMessage(false,eventStrtime,"DEBUG","Command completed");
+							log.info(serverMsg);
+						}else{
+							serverMsg=uf.prettifyMessage(type,eventStrtime,sender,body);
+							console.log(serverMsg);
+						}
+					});
+					if(!event.body)
+							return;
+					if(event.senderID != myId) {
+						var command	 = event.body.split(/ +(?=(?:(?:[^"]*"){2})*[^"]*$)/g);
+						var cmd		 = command[0];
+						var args		= command.slice(1,command.length);
+						var arguments = parseArgs(args,cmd);
+						
+						if(cmdM[cmd] && cmdM[cmd].run){
+							cmdM[cmd].run(event.threadID,arguments,event.senderID);
+					 		api.markAsRead(event.threadID, function(err) {
+								if(err) console.log(err);
+							});
+						}
+					}
+					break;
+				case "event":
+					var serverMsg	= "";
+					var eventStrtime = strftime('%F %T');
+					api.getUserInfo(event.author,function(err,ret){
+						if(err) return console.error(err);
+						serverMsg=uf.prettifyMessage("EVNT",eventStrtime,ret[event.author].name,event.logMessageBody);
+					console.log(serverMsg);
+					});
+				break;
+			}
+		});
 	});
 }
+
 function parseArgs(args,cmd){
 	var arguments = {};
+	var nextSpecial = false;
 	arguments['special']=[];
 	arguments['value']=[];
 	arguments['special_value']={};
 	arguments['cmd']=cmd;
-	//console.log(command);
-	var nextSpecial = false;
+
 	for(i in args){
 		args[i] = args[i].replace(/^"(.*)"$/, '$1');
 		if(args[i].indexOf("--")>-1){
--- src/util_functions.js
+++ src/util_functions.js
@@ -15,16 +15,15 @@ var command = {
 }
 
 String.prototype.replaceAt=function(index, character) {
-    return this.substr(0, index) + character + this.substr(index+character.length);
+	return this.substr(0, index) + character + this.substr(index+character.length);
 }
 String.prototype.capitalizeFirstLetter = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
+	return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 function isInArray(haystack,needle){
 	return haystack.indexOf(needle) > -1;
 }
-
 
 function prettifyMessage(type,time,sender,mesg){
 	var message="";
@@ -76,16 +75,16 @@ function sendPhotoFromUrl(threadID, msg, imgurl, fileName, error,keep){
 		}
 	}).pipe(fs.createWriteStream(fileName)).on('close',function(){
 		var message = {
- 			attachment: fs.createReadStream(fileName),
- 			body: ""
+			attachment: fs.createReadStream(fileName),
+			body: ""
 		};
 		if(msg){
 			message['body']+=msg;
 		}
-    	sendMessage(message, threadID);
-    	if(keep == undefined || keep!=true)
-    		fs.unlink(fileName);
-    	return true;
+		sendMessage(message, threadID);
+		if(keep == undefined || keep!=true)
+			fs.unlink(fileName);
+		return true;
 	});
 }
 
@@ -114,36 +113,36 @@ function getFilesize(filename) {
 
 
 function getEditDistance(a, b){
-  if(a.length == 0) return b.length; 
-  if(b.length == 0) return a.length; 
+	if(a.length == 0) return b.length; 
+	if(b.length == 0) return a.length; 
 
-  var matrix = [];
+	var matrix = [];
 
-  // increment along the first column of each row
-  var i;
-  for(i = 0; i <= b.length; i++){
-    matrix[i] = [i];
-  }
+	// increment along the first column of each row
+	var i;
+	for(i = 0; i <= b.length; i++){
+	matrix[i] = [i];
+	}
 
-  // increment each column in the first row
-  var j;
-  for(j = 0; j <= a.length; j++){
-    matrix[0][j] = j;
-  }
+	// increment each column in the first row
+	var j;
+	for(j = 0; j <= a.length; j++){
+	matrix[0][j] = j;
+	}
 
-  // Fill in the rest of the matrix
-  for(i = 1; i <= b.length; i++){
-    for(j = 1; j <= a.length; j++){
-      if(b.charAt(i-1) == a.charAt(j-1)){
-        matrix[i][j] = matrix[i-1][j-1];
-      } else {
-        matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
-                                Math.min(matrix[i][j-1] + 1, // insertion
-                                         matrix[i-1][j] + 1)); // deletion
-      }
-    }
-  }
-  return matrix[b.length][a.length];
+	// Fill in the rest of the matrix
+	for(i = 1; i <= b.length; i++){
+	for(j = 1; j <= a.length; j++){
+		if(b.charAt(i-1) == a.charAt(j-1)){
+		matrix[i][j] = matrix[i-1][j-1];
+		} else {
+		matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+								Math.min(matrix[i][j-1] + 1, // insertion
+										 matrix[i-1][j] + 1)); // deletion
+		}
+	}
+	}
+	return matrix[b.length][a.length];
 };
 
 function findMostLikelyName(threadID,name){
@@ -185,7 +184,7 @@ function findMostLikelyName(threadID,name){
 		}
 	}
 	var unique_users = likelyUsers.filter(function onlyUnique(value, index, self) { 
-    	return self.indexOf(value) == index;
+		return self.indexOf(value) == index;
 	});
 
 	if(unique_users.length == 1){
@@ -209,13 +208,13 @@ function findMostLikelyName(threadID,name){
 
 
 module.exports = {
-	prettifyMessage   : prettifyMessage,
-	isInArray         : isInArray,
-	getRandomInt      : getRandomInt,
-	getSpecialValue   : getSpecialValue,
-	sendPhotoFromUrl  : sendPhotoFromUrl,
-	sendMessage       : sendMessage,
-	metaData          : command,
-	getFilesize       : getFilesize,
+	prettifyMessage	 : prettifyMessage,
+	isInArray		 : isInArray,
+	getRandomInt	 : getRandomInt,
+	getSpecialValue	 : getSpecialValue,
+	sendPhotoFromUrl : sendPhotoFromUrl,
+	sendMessage		 : sendMessage,
+	metaData		 : command,
+	getFilesize		 : getFilesize,
 	findMostLikelyName: findMostLikelyName
 }
